0545933d26cfdc2823a0151ae36d70d3634ca4d2
Denakpo Agba <agbagerard@gmail.com>
2025-03-05 12:12:56 -0500

    Authform for register and login

---
+++ src/lib/components/AuthForm.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import { Button } from '$components';
+
+	interface ComponentProps {
+		isRegistration: boolean;
+	}
+
+	let { isRegistration }: ComponentProps = $props();
+</script>
+
+<div class="default-margin auth-container">
+	<h1 class="mb-l">{isRegistration ? 'Register' : 'Login'}</h1>
+	<div class="form-and-social-login">
+		<form class="auth-form">
+			{#if isRegistration}
+				<input placeholder="Name" type="text" name="name" />
+			{/if}
+			<input placeholder="Email" type="text" name="email" />
+			<input placeholder="Password" type="password" name="password" />
+			{#if isRegistration}
+				<input placeholder="Confirm password" type="password" name="passwordConfirmation" />
+			{/if}
+			<Button type="submit">{isRegistration ? 'Register' : 'Login'}</Button>
+			{#if isRegistration}
+				<p class="auth-hint mt-s">Already have an account? <a href="/login">Log In</a></p>
+			{:else}
+				<p class="auth-hint mt-s">If you don't have an account? <a href="/register">Sign up</a></p>
+			{/if}
+		</form>
+		<div class="social-login">
+			<!-- Add a button here -->
+		</div>
+	</div>
+</div>
