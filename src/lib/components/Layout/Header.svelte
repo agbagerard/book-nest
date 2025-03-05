0620381816516baecd0acb0e3fd7fa0ab687cb3c
Denakpo Agba <agbagerard@gmail.com>
2025-03-05 18:01:08 -0500

    Set up context to keep track of user state

--- src/lib/components/Layout/Header.svelte
+++ src/lib/components/Layout/Header.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
 	import bookNestLogo from '$assets/app-logo.svg';
 	import { Button } from '$components';
+	import { getUserState } from '$components/state/user-state.svelte';
+
+	let userContext = getUserState();
+	let { user } = $derived(userContext);
+
+	$inspect(user);
 </script>
 
 <header>
@@ -8,14 +14,25 @@
 		<img class="logo" src={bookNestLogo} alt="Got to home" />
 	</a>
 	<nav>
-		<ul>
-			<li>
-				<Button isMenu={true} href="/register">Create account</Button>
-			</li>
-			<li>
-				<Button isMenu={true} isSecondary={true} href="/login">Login</Button>
-			</li>
-		</ul>
+		{#if !user}
+			<ul>
+				<li>
+					<Button isMenu={true} href="/register">Create account</Button>
+				</li>
+				<li>
+					<Button isMenu={true} isSecondary={true} href="/login">Login</Button>
+				</li>
+			</ul>
+		{:else}
+			<ul>
+				<li>
+					{user.email}
+				</li>
+				<li>
+					<Button isMenu={true} onclick={() => userContext.logout()}>Logout</Button>
+				</li>
+			</ul>
+		{/if}
 	</nav>
 </header>
 
@@ -33,6 +50,7 @@
 
 	ul {
 		display: flex;
+		align-items: center;
 		column-gap: 24px;
 	}
 </style>
