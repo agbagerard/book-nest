4eac34eaf69330f3e19334c71e0bab85141f092a
Denakpo Agba <agbagerard@gmail.com>
2025-03-05 10:18:14 -0500

    Herosection styling

--- src/lib/components/HeroSection.svelte
+++ src/lib/components/HeroSection.svelte
@@ -18,3 +18,25 @@
 	</div>
 	<img class="hero-image" src={heroImage} alt="" />
 </section>
+
+<style>
+	.hero {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		margin-top: 100px;
+		margin-bottom: 80px;
+	}
+
+	.hero-image {
+		width: 40%;
+	}
+
+	.hero-text {
+		display: flex;
+		flex-direction: column;
+		align-items: flex-start;
+		width: 55%;
+		padding-right: 50px;
+	}
+</style>
