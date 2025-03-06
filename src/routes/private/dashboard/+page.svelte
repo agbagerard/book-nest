0931dbdb616f1815ade48d4509e1529f0e34ffd2
Denakpo Agba <agbagerard@gmail.com>
2025-03-06 12:50:02 -0500

    user dashboard

--- src/routes/private/dashboard/+page.svelte
+++ src/routes/private/dashboard/+page.svelte
@@ -1,14 +1,24 @@
 <script lang="ts">
 	import { getUserState } from '$lib/state/user-state.svelte';
+	import Icon from '@iconify/svelte';
 
 	let userContext = getUserState();
-	let { allBooks } = $derived(userContext);
-
-	$inspect(allBooks);
+	let { userName } = $derived(userContext);
 </script>
 
-<ul>
-	{#each allBooks as book}
-		<li>{book.title}</li>
-	{/each}
-</ul>
+<div class="dashboard">
+	<div class="dashboard-header mb-m">
+		<a href="/private/scan-shelf" class="add-book">
+			<Icon icon="gala:add" width={'72'} height={'72'} />
+			<p>Add a book</p>
+		</a>
+		<div class="headline">
+			<h3 class="bold mb-xs">Welcome Back, {userName}</h3>
+			<p>
+				There's nothing quite like the journey a good book can take you on. Have you discovered any
+				new favorites recently?
+			</p>
+		</div>
+	</div>
+	<!-- BookCategories -->
+</div>
