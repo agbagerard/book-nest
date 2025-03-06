01081f4d7f4c730f30195548ac5f251e1fd4445b
Denakpo Agba <agbagerard@gmail.com>
2025-03-06 12:30:34 -0500

    Side navigation and Auth layout

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -3,3 +3,4 @@ export { default as HeroSection } from './HeroSection.svelte';
 export { default as Button } from './Button.svelte';
 export { default as Header } from './Layout/Header.svelte';
 export { default as AuthForm } from './AuthForm.svelte';
+export { default as SideNav } from './Layout/SideNavigation.svelte';
---
+++ src/routes/private/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { SideNav } from '$components';
+	let { children } = $props();
+</script>
+
+<div class="auth-area">
+	<SideNav />
+	<div class="main-area">
+		{@render children()}
+	</div>
+</div>
