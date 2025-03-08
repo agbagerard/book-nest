d3e5d202128bc51651c2f6ead84459540a72e2cf
Denakpo Agba <agbagerard@gmail.com>
2025-03-07 19:03:30 -0500

    Add books to database through scan-shelf page

--- src/lib/state/user-state.svelte.ts
+++ src/lib/state/user-state.svelte.ts
@@ -9,6 +9,11 @@ interface UserStateProps {
 	user: User | null;
 }
 
+export interface OpenAiBook {
+	author: string;
+	bookTitle: string;
+}
+
 export interface Book {
 	author: string | null;
 	cover_image: string | null;
@@ -183,6 +188,34 @@ export class UserState {
 		}
 
 		goto('/private/dashboard');
+	}
+
+	async addBooksToLibrary(booksToAdd: OpenAiBook[]) {
+		if (!this.supabase || !this.user) {
+			return;
+		}
+
+		const userId = this.user.id;
+
+		const processedBooks = booksToAdd.map((book) => ({
+			title: book.bookTitle,
+			author: book.author,
+			user_id: userId
+		}));
+
+		const { error } = await this.supabase.from('books').insert(processedBooks);
+
+		if (error) {
+			throw new Error(error.message);
+		} else {
+			const { data } = await this.supabase.from('books').select('*').eq('user_id', userId);
+
+			if (!data) {
+				throw new Error('Could not retrieve all books for user');
+			}
+
+			this.allBooks = data;
+		}
 	}
 
 	async logout() {
--- src/routes/private/scan-shelf/+page.svelte
+++ src/routes/private/scan-shelf/+page.svelte
@@ -1,18 +1,15 @@
 <script lang="ts">
 	import { Button } from '$components';
+	import { getUserState, type OpenAiBook } from '$lib/state/user-state.svelte';
 	import { convertFileToBase64 } from '$lib/utils/openai-helpers';
 	import Icon from '@iconify/svelte';
 	import Dropzone from 'svelte-file-dropzone';
 
+	let userContext = getUserState();
 	let isLoading = $state(false);
 	let errorMessage = $state('');
 	let recognizedBooks = $state<OpenAiBook[]>([]);
 	let booksSuccessfullyAdded = $state(false);
-
-	interface OpenAiBook {
-		author: string;
-		bookTitle: string;
-	}
 
 	async function handleDrop(e: CustomEvent<any>) {
 		const { acceptedFiles } = e.detail;
@@ -41,6 +38,21 @@
 			}
 		} else {
 			errorMessage = `Could not upload given file. Are you sure it's an image with a file size of less than 10MB?`;
+		}
+	}
+
+	function removeBook(index: number) {
+		recognizedBooks.splice(index, 1);
+	}
+
+	async function addAllBooks() {
+		isLoading = true;
+		try {
+			await userContext.addBooksToLibrary(recognizedBooks);
+			isLoading = false;
+			booksSuccessfullyAdded = true;
+		} catch (error: any) {
+			errorMessage = error.message;
 		}
 	}
 </script>
@@ -84,7 +96,7 @@
 				</tr>
 			</thead>
 			<tbody>
-				{#each recognizedBooks as book, i}
+				{#each recognizedBooks as book, index}
 					<tr>
 						<td>{book.bookTitle}</td>
 						<td>{book.author}</td>
@@ -93,7 +105,7 @@
 								type="button"
 								aria-label="Remove book"
 								class="remove-book"
-								onclick={() => console.log(`Delete book with index ${i}`)}
+								onclick={() => removeBook(index)}
 							>
 								<Icon icon="streamline:delete-1-solid" width={'24'} />
 							</button>
@@ -102,9 +114,7 @@
 				{/each}
 			</tbody>
 		</table>
-		<Button isMenu={true} onclick={() => console.log('add all remaining books')}
-			>Add all books</Button
-		>
+		<Button isMenu={true} onclick={addAllBooks}>Add all books</Button>
 	</div>
 {:else}
 	<h4>The selected {recognizedBooks.length} books have been added to your library.</h4>
